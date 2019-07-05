542c7fb84fc7d21e25531a3dbe171b989c3ff881
vakitdaraliyor <osmansavas93@hotmail.com>
2019-07-05 18:36:25 +0300

    CRUD Operations

--- CSharp/ADO.Net/ADO.Net/Form1.Designer.cs
+++ CSharp/ADO.Net/ADO.Net/Form1.Designer.cs
@@ -28,35 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txtKATEGORI_REFNO = new System.Windows.Forms.TextBox();
+            this.txtKATEGORI_ADI = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(83, 101);
+            this.dataGridView1.Location = new System.Drawing.Point(83, 147);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(635, 312);
+            this.dataGridView1.Size = new System.Drawing.Size(407, 266);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_RowEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kategori RefNo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kategori Adı";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Yeni";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(193, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(301, 113);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Vazgeç";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(415, 113);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Sil";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // txtKATEGORI_REFNO
+            // 
+            this.txtKATEGORI_REFNO.Location = new System.Drawing.Point(177, 36);
+            this.txtKATEGORI_REFNO.Name = "txtKATEGORI_REFNO";
+            this.txtKATEGORI_REFNO.ReadOnly = true;
+            this.txtKATEGORI_REFNO.Size = new System.Drawing.Size(199, 20);
+            this.txtKATEGORI_REFNO.TabIndex = 7;
+            // 
+            // txtKATEGORI_ADI
+            // 
+            this.txtKATEGORI_ADI.Location = new System.Drawing.Point(177, 73);
+            this.txtKATEGORI_ADI.MaxLength = 20;
+            this.txtKATEGORI_ADI.Name = "txtKATEGORI_ADI";
+            this.txtKATEGORI_ADI.Size = new System.Drawing.Size(199, 20);
+            this.txtKATEGORI_ADI.TabIndex = 8;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtKATEGORI_ADI);
+            this.Controls.Add(this.txtKATEGORI_REFNO);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtKATEGORI_REFNO;
+        private System.Windows.Forms.TextBox txtKATEGORI_ADI;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
 
--- CSharp/ADO.Net/ADO.Net/Form1.cs
+++ CSharp/ADO.Net/ADO.Net/Form1.cs
@@ -13,6 +13,10 @@ namespace ADO.Net
 {
     public partial class Form1 : Form
     {
+         
+        SqlConnection connection = new SqlConnection(@"Data Source=localhost\SQLEXPRESS; initial catalog=İŞKUR; user id=sa; password=1234");
+        int satir = -1;
+
         public Form1()
         {
             InitializeComponent();
@@ -20,20 +24,127 @@ namespace ADO.Net
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //Data Source = myServerAddress; Initial Catalog = myDataBase; Integrated Security = SSPI;
-            //User ID = myDomain\myUsername; Password = myPassword;
-            // Veritabanına bağlandı
-            String cnnstr = @"Data Source=localhost\SQLEXPRESS;initial catalog=İŞKUR; user id=sa; password=1234";
-            SqlConnection connection = new SqlConnection(cnnstr);
+            GridDoldur();
+        }
+
+        private void DataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            satir = e.RowIndex;
+            if(satir > -1)
+            {
+                txtKATEGORI_REFNO.Text = dataGridView1.Rows[satir].Cells["KATEGORI_REFNO"].Value.ToString();
+                txtKATEGORI_ADI.Text = dataGridView1.Rows[satir].Cells["KATEGORI_ADI"].Value.ToString();
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //Yeni(Button)
+            txtKATEGORI_REFNO.Text = "";
+            txtKATEGORI_ADI.Text = "";
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            //Vazgeç
+            if (satir > -1)
+            {
+                txtKATEGORI_REFNO.Text = dataGridView1.Rows[satir].Cells["KATEGORI_REFNO"].Value.ToString();
+                txtKATEGORI_ADI.Text = dataGridView1.Rows[satir].Cells["KATEGORI_ADI"].Value.ToString();
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //Kaydet
+
+            if(txtKATEGORI_ADI.Text.Trim() == "")
+            {
+                errorProvider1.SetError(txtKATEGORI_ADI, "Kategori Adı giriniz");
+                return;
+            }
+            else
+            {
+                errorProvider1.SetError(txtKATEGORI_ADI, "");
+            }
+
+            SqlParameter prm1 = new SqlParameter("@p1", txtKATEGORI_ADI.Text);
+            SqlParameter prm2 = new SqlParameter("@p2", txtKATEGORI_REFNO.Text);
+
+            string sql = "";
+            if(txtKATEGORI_REFNO.Text == "")
+            {
+                sql = "INSERT INTO KATEGORI(KATEGORI_ADI) VALUES(@p1)";
+            }
+            else
+            {
+                sql = "UPDATE KATEGORI SET KATEGORI_ADI = '@p1' WHERE KATEGORI_REFNO = @p2";
+            }
+
+            SqlCalistir(sql, prm1, prm2);            
+
+            connection.Close();
+
+            GridDoldur();
+
+            MessageBox.Show("Başarıyla eklendi");
+
+        }
+
+        void GridDoldur()
+        {
             // Komut oluşturup connection oluşturuyoruz.
             SqlCommand command = new SqlCommand("SELECT * FROM KATEGORI", connection);
             // Veri taşıma işlemlerini yapan bir adapter oluşturuyoruz.
             SqlDataAdapter adapter = new SqlDataAdapter(command);
 
             DataTable dt = new DataTable();
-            
             adapter.Fill(dt); // sql cümlesini çalıştırıp dt tablosuna doldur
             dataGridView1.DataSource = dt;
         }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            // Silme
+            string sql = "";
+
+            DialogResult dialogResult = MessageBox.Show("Silmek istediğinize emin misiniz?", "Silme", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialogResult == DialogResult.Yes)
+            {
+                
+                if (txtKATEGORI_REFNO.Text != "")
+                {
+                    sql = "DELETE FROM KATEGORI WHERE KATEGORI_REFNO = @p2";
+                    SqlParameter prm2 = new SqlParameter("@p2", txtKATEGORI_REFNO.Text);
+
+                    SqlCalistir(sql, prm2);
+                    GridDoldur();
+
+                    MessageBox.Show("Başarıyla silindi");
+                }
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                
+            }     
+
+        }
+
+        private void SqlCalistir(string sql, params SqlParameter[] prms)
+        {
+
+            SqlCommand cmd = new SqlCommand(sql, connection);
+
+            if(prms != null) cmd.Parameters.AddRange(prms);
+       
+            if (connection.State == ConnectionState.Closed) connection.Open();
+            
+            cmd.ExecuteNonQuery(); // INSERT UPDATE DELETE komutlarını çalıştırır                
+
+            connection.Close();
+
+            GridDoldur();
+        }
+
     }
 }
