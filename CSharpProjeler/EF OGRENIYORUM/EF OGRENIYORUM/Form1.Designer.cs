3ac06e23f5be5963cdf2b0864e71e570b88b970f
vakitdaraliyor <osmansavas93@hotmail.com>
2019-07-22 20:11:10 +0300

    Entity FW and LINQ

---
+++ CSharpProjeler/EF OGRENIYORUM/EF OGRENIYORUM/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace EF_OGRENIYORUM
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(228, 18);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(560, 420);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(42, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(42, 96);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(42, 126);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(42, 155);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "where";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(42, 184);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "select";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(42, 213);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(42, 242);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(42, 271);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "order by";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(42, 299);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "order by 2";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(42, 329);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "orderbythenby";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.Button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(42, 358);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "orderbyshort";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.Button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(42, 388);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "Sql Like";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.Button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(147, 37);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 24);
+            this.button14.TabIndex = 14;
+            this.button14.Text = "Select many";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.Button14_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+    }
+}
+
---
+++ CSharpProjeler/EF OGRENIYORUM/EF OGRENIYORUM/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EF_OGRENIYORUM
+{
+    public partial class Form1 : Form
+    {
+        PRATIKEntities db = new PRATIKEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var liste = db.URUNs.ToList(); // ToList() dersek sorgulama baslar ve biter
+            listBox1.Items.Clear();
+            for (int i = 0; i < liste.Count; i++)
+            {
+                listBox1.Items.Add(liste[i].ADI);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            var liste = db.URUNs; // sadece sorgu tanımı (ToList() yok)
+
+            foreach (var item in liste) // sorgulama baslıyor.
+            {
+                listBox1.Items.Add(item.ADI);
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            // Butun kolonlari secmek istemiyorum
+            var liste = db.URUNs.Select(u => new { u.ADI, u.KDVSIZ_SATIS_FIYATI});
+            foreach (var item in liste)
+            {
+                listBox1.Items.Add(item.ADI + " " + item.KDVSIZ_SATIS_FIYATI.ToString());
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            var liste = from urun in db.URUNs select urun; // SQL LIKE LINQ
+            foreach (var item in liste)
+            {
+                listBox1.Items.Add(item.ADI + " " + item.KDVSIZ_SATIS_FIYATI.ToString());
+            }
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            string adi = "ASUS"; // SQL injection önlemek için bu sekilde tanımladık
+            var liste = db.URUNs.Where(u => (u.KDVSIZ_SATIS_FIYATI > 10 && u.KDVSIZ_SATIS_FIYATI < 100) && u.ADI.Contains(adi)); // Tanımlandı 
+            // adi = "HP" adi HP olarak alır. Çünkü Yukarda henüz çalıştırılmadı sadece tanımlandı.
+            foreach (var item in liste) // Çalıştı
+            {
+                listBox1.Items.Add(item.ADI + " " + item.KDVSIZ_SATIS_FIYATI.ToString());
+            }
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            string adi = "ASUS"; // SQL injection önlemek için bu sekilde tanımladık
+            var liste = db.URUNs.Select(u => new { UrunAdi = u.ADI, Fiyati = u.KDVSIZ_SATIS_FIYATI}) // column adlari değişti
+                                .Where(u => (u.Fiyati > 10 && u.Fiyati < 100) && u.UrunAdi.Contains(adi)); // Tanımlandı (değişen column isimleriyle devam edildi)
+            
+            foreach (var item in liste) // Çalıştı
+            {
+                listBox1.Items.Add(item.UrunAdi + " " + item.Fiyati.ToString());
+            }
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            string adi = "ASUS";
+            var liste = db.URUNs.Where(u => (u.KDVSIZ_SATIS_FIYATI > 10 && u.KDVSIZ_SATIS_FIYATI < 100) && u.ADI.Contains(adi))
+                                .Select(u => new { UrunAdi = u.ADI, Fiyati = u.KDVSIZ_SATIS_FIYATI });
+                                
+            foreach (var item in liste) // Çalıştı
+            {
+                listBox1.Items.Add(item.UrunAdi + " " + item.Fiyati.ToString());
+            }
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            string adi = "ASUS";
+            var liste = from urun in db.URUNs
+                        where (urun.KDVSIZ_SATIS_FIYATI > 10 && urun.KDVSIZ_SATIS_FIYATI < 100) || urun.ADI.Contains(adi)
+                        select new { UrunAdi = urun.ADI, Fiyati = urun.KDVSIZ_SATIS_FIYATI };
+            adi = "HP";
+            foreach (var item in liste) // Çalıştı
+            {
+                listBox1.Items.Add(item.UrunAdi + " " + item.Fiyati.ToString());
+            }
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            var liste = db.URUNs.OrderBy(u => u.ADI);
+            foreach (var item in liste)
+            {
+                listBox1.Items.Add(item.ADI + " " + item.KDVSIZ_SATIS_FIYATI.ToString());
+            }
+        }
+
+        private void Button10_Click(object sender, EventArgs e)
+        {
+            var liste = db.URUNs.OrderBy(u => u.KATEGORI_REFNO).ThenBy(u => u.KDVSIZ_SATIS_FIYATI);
+            foreach (var item in liste)
+            {
+                listBox1.Items.Add(item.KATEGORI_REFNO.ToString() + " " + item.KDVSIZ_SATIS_FIYATI.ToString());
+            }
+        }
+
+        private void Button11_Click(object sender, EventArgs e)
+        {
+            var liste = db.URUNs.OrderByDescending(u => u.KATEGORI_REFNO).ThenByDescending(u => u.KDVSIZ_SATIS_FIYATI);
+            foreach (var item in liste)
+            {
+                listBox1.Items.Add(item.KATEGORI_REFNO.ToString() + " " + item.KDVSIZ_SATIS_FIYATI.ToString());
+            }
+        }
+
+        private void Button12_Click(object sender, EventArgs e)
+        {
+            var liste = db.URUNs.OrderBy(u => new { u.KATEGORI_REFNO, u.KDVSIZ_ALIS_FIYATI });
+            foreach (var item in liste)
+            {
+                listBox1.Items.Add(item.KATEGORI_REFNO.ToString() + " " + item.KDVSIZ_SATIS_FIYATI.ToString());
+            }
+        }
+
+        private void Button13_Click(object sender, EventArgs e)
+        {
+            var liste = from urun in db.URUNs
+                        where urun.KDVSIZ_SATIS_FIYATI > 100
+                        orderby urun.KATEGORI_REFNO, urun.KDVSIZ_SATIS_FIYATI
+                        select urun;
+            foreach (var item in liste)
+            {
+                listBox1.Items.Add(item.KATEGORI_REFNO.ToString() + " " + item.KDVSIZ_SATIS_FIYATI.ToString());
+            }
+        }
+
+        private void Button14_Click(object sender, EventArgs e)
+        {
+            // SQL => SELECT * FROM JOIN KATEGORI ON URUN.KATEGORI_REFNO=KATEGORI.KATEGORI_REFNO
+            var liste = db.KATEGORIs.SelectMany(d => d.URUNs, (k, u) => new { k.KATEGORI_ADI, u.ADI });
+            foreach (var item in liste)
+            {
+                listBox1.Items.Add("Kategori Adı: " + item.KATEGORI_ADI.ToString() + " Urun Adı: " + item.ADI.ToString());
+            }
+        }
+    }
+}
