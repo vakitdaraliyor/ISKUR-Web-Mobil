375a97941716abd2915fc7ce0cf0bbf84af1827a
Osman Savas <osmansavas93@hotmail.com>
2019-09-05 14:53:56 +0300

    GUNLUK Yorum.aspx added

--- Mustafa Hoca Part2/GUNLUK/GUNLUK/admin/Kategori.aspx
+++ Mustafa Hoca Part2/GUNLUK/GUNLUK/admin/Kategori.aspx
@@ -27,22 +27,22 @@
             <tr>
                 <td>
                     <asp:GridView ID="GridView1" runat="server" AllowPaging="True" AutoGenerateColumns="False" CellPadding="4" DataKeyNames="KATEGORI_REFNO" ForeColor="#333333" GridLines="None" OnPageIndexChanging="GridView1_PageIndexChanging" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" PageSize="5" Width="100%">
-                        <AlternatingRowStyle BackColor="White" />
+                        <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
                         <Columns>
                             <asp:CommandField HeaderText="Seç" ShowSelectButton="True" />
                             <asp:BoundField DataField="KATEGORI_REFNO" HeaderText="Kategori REFNO" />
                             <asp:BoundField DataField="KATEGORI_ADI" HeaderText="Kategori Adı" />
                         </Columns>
-                        <EditRowStyle BackColor="#2461BF" />
-                        <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
-                        <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
-                        <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
-                        <RowStyle BackColor="#EFF3FB" />
-                        <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
-                        <SortedAscendingCellStyle BackColor="#F5F7FB" />
-                        <SortedAscendingHeaderStyle BackColor="#6D95E1" />
-                        <SortedDescendingCellStyle BackColor="#E9EBEF" />
-                        <SortedDescendingHeaderStyle BackColor="#4870BE" />
+                        <EditRowStyle BackColor="#999999" />
+                        <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                        <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                        <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+                        <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+                        <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+                        <SortedAscendingCellStyle BackColor="#E9E7E2" />
+                        <SortedAscendingHeaderStyle BackColor="#506C8C" />
+                        <SortedDescendingCellStyle BackColor="#FFFDF8" />
+                        <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
                     </asp:GridView>
                 </td>
             </tr>
---
+++ Mustafa Hoca Part2/GUNLUK/GUNLUK/admin/Yorum.aspx.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GUNLUK.admin
+{
+    public partial class Yorum : System.Web.UI.Page
+    {
+        BLOGEntities entities = new BLOGEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["GIRIS_YETKI"] == null)
+            {
+                Response.Redirect("Giris.aspx");
+            }
+            else
+            {
+                if (Convert.ToBoolean(Session["GIRIS_YETKI"]) == false)
+                {
+                    Response.Redirect("Giris.aspx");
+                }
+            }
+
+            GridView1.DataSource = entities.YORUM.ToList();
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Seç
+            int refno = Convert.ToInt32(GridView1.SelectedDataKey.Value);
+            YORUM y = entities.YORUM.Find(refno);
+
+            if (y != null)
+            {
+                txtYORUM_REFNO.Text = Convert.ToString(y.YORUM_REFNO);
+                txtYAZI_REFNO.Text = Convert.ToString(y.YAZI_REFNO);
+                txtMAIL.Text = y.MAIL;
+                txtADI_SOYADI.Text = y.ADI_SOYADI;
+                drpDURUMU.SelectedValue = y.DURUMU.ToString();
+                txtTARIH.Text = y.TARIH.ToString().Substring(0, 10).Replace("/", "-");
+                txtIP.Text = y.IP;
+                txtYORUM_ICERIK.Text = y.YORUM_ICERIK;
+            }
+
+            // kayit panelini aç liste panelini kapat
+            pnlKAYIT.Visible = true;
+            pnlLISTE.Visible = false;
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            // Kaydet
+            if (txtYORUM_REFNO.Text != "")
+            {
+                YORUM y = entities.YORUM.Find(Convert.ToInt32(txtYORUM_REFNO.Text));
+                y.YAZI_REFNO = Convert.ToInt32(txtYAZI_REFNO.Text);
+                y.MAIL = txtMAIL.Text;
+                y.ADI_SOYADI = txtADI_SOYADI.Text;
+                y.DURUMU = Convert.ToBoolean(drpDURUMU.SelectedValue);
+                y.TARIH = Convert.ToDateTime(txtTARIH.Text);
+                y.IP = txtIP.Text;
+                y.YORUM_ICERIK = txtYORUM_ICERIK.Text;
+
+                entities.SaveChanges();
+            }
+            else
+            {
+                YORUM y = new YORUM();
+                y.YAZI_REFNO = Convert.ToInt32(txtYAZI_REFNO.Text);
+                y.MAIL = txtMAIL.Text;
+                y.ADI_SOYADI = txtADI_SOYADI.Text;
+                y.DURUMU = Convert.ToBoolean(drpDURUMU.SelectedValue);
+                y.TARIH = Convert.ToDateTime(txtTARIH.Text);
+                y.IP = txtIP.Text;
+                y.YORUM_ICERIK = txtYORUM_ICERIK.Text;
+                entities.YORUM.Add(y);
+
+                entities.SaveChanges();
+            }
+
+            GridView1.DataSource = entities.YORUM.ToList();
+            GridView1.DataBind();
+
+            pnlKAYIT.Visible = false;
+            pnlLISTE.Visible = true;
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            // Vazgeç
+            GridView1.DataSource = entities.YORUM.ToList();
+            GridView1.DataBind();
+
+            pnlLISTE.Visible = true;
+            pnlKAYIT.Visible = false;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            // Yeni
+            txtYORUM_REFNO.Text = "";
+            txtYAZI_REFNO.Text = "";
+            txtMAIL.Text = "";
+            txtADI_SOYADI.Text = "";
+            drpDURUMU.SelectedValue = "True";
+            txtTARIH.Text = "";
+            txtIP.Text = "";
+            txtYORUM_ICERIK.Text = "";
+
+            pnlKAYIT.Visible = true;
+            pnlLISTE.Visible = false;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // Ara
+            GridView1.DataSource = entities.YORUM.Where(y => y.YORUM_ICERIK.Contains(txtYORUM_ARA.Text)).ToList();
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            // PageIndexChanging
+            GridView1.PageIndex = e.NewPageIndex; // tiklanan sayfa
+            GridView1.DataSource = entities.YORUM.Where(y => y.YORUM_ICERIK.Contains(txtYORUM_ARA.Text)).ToList();
+            GridView1.DataBind();
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            // Sil
+            if (txtYORUM_REFNO.Text != "")
+            {
+                YORUM y = entities.YORUM.Find(Convert.ToInt32(txtYORUM_REFNO.Text));
+                entities.YORUM.Remove(y);
+
+                entities.SaveChanges();
+            }
+
+            GridView1.DataSource = entities.YORUM.ToList();
+            GridView1.DataBind();
+
+            pnlKAYIT.Visible = false;
+            pnlLISTE.Visible = true;
+        }
+    }
+}
