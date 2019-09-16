afc26dd4c05b51b956a3fe88edbe0b459fd573b1
Osman Savas <osmansavas93@hotmail.com>
2019-09-16 09:00:10 +0300

    Firma_MVC Updated

--- Mustafa Hoca Part2/Firma_MVC/Firma_MVC/Areas/Admin/Controllers/ProjelerController.cs
+++ Mustafa Hoca Part2/Firma_MVC/Firma_MVC/Areas/Admin/Controllers/ProjelerController.cs
@@ -1,129 +1,83 @@
-﻿using System;
+﻿using Firma_MVC.Models;
+using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
 using System.Linq;
-using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using Firma_MVC.Models;
 
 namespace Firma_MVC.Areas.Admin.Controllers
 {
     public class ProjelerController : Controller
     {
-        private FIRMAMODEL db = new FIRMAMODEL();
-
+        FIRMAMODEL db = new FIRMAMODEL();
         // GET: Admin/Projeler
-        public ActionResult Index()
+        public ActionResult Index(string arama)
         {
-            return View(db.PROJEs.ToList());
+            List<PROJE> liste = new List<PROJE>();
+            if (arama == null)
+            {
+                arama = "";
+                liste = db.PROJEs.ToList();
+            }
+            else
+            {
+                liste = db.PROJEs.Where(p => p.PROJE_ADI.Contains(arama)).ToList();
+            }
+            ViewData["veri"] = arama;
+            return View(liste);
         }
 
-        // GET: Admin/Projeler/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PROJE pROJE = db.PROJEs.Find(id);
-            if (pROJE == null)
-            {
-                return HttpNotFound();
-            }
-            return View(pROJE);
-        }
-
-        // GET: Admin/Projeler/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Admin/Projeler/Create
-        // Aşırı gönderim saldırılarından korunmak için, lütfen bağlamak istediğiniz belirli özellikleri etkinleştirin, 
-        // daha fazla bilgi için https://go.microsoft.com/fwlink/?LinkId=317598 sayfasına bakın.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [ValidateInput(false)]
-        public ActionResult Create([Bind(Include = "PROJE_REFNO,PROJE_ADI,RESIM,ACIKLAMA")] PROJE pROJE)
-        {
-            if (ModelState.IsValid)
-            {
-                db.PROJEs.Add(pROJE);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(pROJE);
-        }
-
-        // GET: Admin/Projeler/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PROJE pROJE = db.PROJEs.Find(id);
-            if (pROJE == null)
-            {
-                return HttpNotFound();
-            }
-            return View(pROJE);
-        }
-
-        // POST: Admin/Projeler/Edit/5
-        // Aşırı gönderim saldırılarından korunmak için, lütfen bağlamak istediğiniz belirli özellikleri etkinleştirin, 
-        // daha fazla bilgi için https://go.microsoft.com/fwlink/?LinkId=317598 sayfasına bakın.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [ValidateInput(false)]
-        public ActionResult Edit([Bind(Include = "PROJE_REFNO,PROJE_ADI,RESIM,ACIKLAMA")] PROJE pROJE)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(pROJE).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(pROJE);
-        }
-
-        // GET: Admin/Projeler/Delete/5
         public ActionResult Delete(int? id)
         {
-            if (id == null)
+            if (id != null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                PROJE p = db.PROJEs.Find(id);
+                if (p != null)
+                {
+                    db.PROJEs.Remove(p);
+                }
             }
-            PROJE pROJE = db.PROJEs.Find(id);
-            if (pROJE == null)
-            {
-                return HttpNotFound();
-            }
-            return View(pROJE);
-        }
-
-        // POST: Admin/Projeler/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            PROJE pROJE = db.PROJEs.Find(id);
-            db.PROJEs.Remove(pROJE);
-            db.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        protected override void Dispose(bool disposing)
+        [HttpGet]
+        public ActionResult Create(int? id)
         {
-            if (disposing)
+            PROJE p = new PROJE();
+            if (id != null)
             {
-                db.Dispose();
+                p = db.PROJEs.Find(id);
+                if (p == null)
+                {
+                    p = new PROJE();
+                }
             }
-            base.Dispose(disposing);
+            return View(p);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(PROJE p)
+        {
+            if (ModelState.IsValid)
+            {
+                if (p.PROJE_REFNO == 0)
+                {
+                    db.PROJEs.Add(p);
+                }
+                else
+                {
+                    db.Entry(p).State = System.Data.Entity.EntityState.Modified;
+                }
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+
+        public ActionResult Search(string txtARA)
+        {
+            return RedirectToAction("Index", "Projeler", new { arama = txtARA });
         }
     }
 }
