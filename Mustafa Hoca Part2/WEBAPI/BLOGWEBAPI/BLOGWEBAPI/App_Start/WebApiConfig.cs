87e6e33bb94e61efdeafa78cfc8854962ef17793
vakitdaraliyor <osmansavas93@hotmail.com>
2019-10-23 21:44:38 +0300

    WEBAPI Eklendi

---
+++ Mustafa Hoca Part2/WEBAPI/BLOGWEBAPI/BLOGWEBAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace BLOGWEBAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API yapılandırması ve hizmetleri
+
+            // Web API yolları
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            // Sonuclar XML olarak verilmez
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+
+        }
+    }
+}
---
+++ Mustafa Hoca Part2/WEBAPI/WINDOWSBLOGAPI/WINDOWSBLOGAPI/KULLANICI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WINDOWSBLOGAPI
+{
+    public class KULLANICI
+    {
+        public int KULLANICI_REFNO { get; set; }
+
+        public string KULLANICI_ADI { get; set; }
+
+        public string PAROLA { get; set; }
+
+        public bool DURUMU { get; set; }
+    }    
+}
